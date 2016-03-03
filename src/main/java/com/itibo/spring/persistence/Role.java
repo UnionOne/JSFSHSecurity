e11f9146a4b6c565972b3710f317f8f926481f0a
Union One <hax2033@gmail.com>
2016-03-03 12:11:22 +0300

    Create hibernate configs
    Added Role and User classes
    Role.hbm.xml - config of ROLE table
    User.hbm.xml - config of USER table
    hibernate.cfg.xml - mapping of two files over there

---
+++ src/main/java/com/itibo/spring/persistence/Role.java
@@ -0,0 +1,44 @@
+package com.itibo.spring.persistence;
+
+/**
+ * Created by union on 03.03.2016.
+ */
+
+public class Role {
+    private int roleId;
+    private String code;
+    private String label;
+    private User user;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/itibo/spring/persistence/User.java
@@ -0,0 +1,56 @@
+package com.itibo.spring.persistence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by union on 03.03.2016.
+ */
+
+public class User {
+    private int userId;
+    private String login;
+    private String pwd;
+    private Integer enabled;
+    private Set<Role> roles = new HashSet<>(0);
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public Integer getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Integer enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
