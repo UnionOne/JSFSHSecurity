1b26bb639e5452990e4c7e2c3263e0b56bd1c28a
Union One <hax2033@gmail.com>
2016-03-09 12:24:27 +0300

    Create service class

---
+++ src/main/java/com/itibo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.itibo.service;
+
+import com.itibo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void addUser(User user);
+
+    void deleteUser(User user);
+
+    User getUserByLogin(String login);
+
+    List getAllUsers();
+}
---
+++ src/main/java/com/itibo/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.itibo.service;
+
+import com.itibo.dao.UserDAO;
+import com.itibo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public void addUser(User user) {
+        this.userDAO.addUser(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        this.userDAO.deleteUser(user);
+    }
+
+    @Override
+    public User getUserByLogin(String login) {
+        return userDAO.getUserByLogin(login);
+    }
+
+    @Override
+    public List getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+}
