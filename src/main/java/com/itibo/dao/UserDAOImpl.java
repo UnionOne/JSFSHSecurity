e835c13941160927b24971e1acbcf9b0874777a6
Union One <hax2033@gmail.com>
2016-03-09 12:20:48 +0300

    Create DAO methods

--- src/main/java/com/itibo/dao/UserDAOImpl.java
+++ src/main/java/com/itibo/dao/UserDAOImpl.java
@@ -1,10 +1,38 @@
 package com.itibo.dao;
 
 import com.itibo.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class UserDAOImpl implements UserDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void addUser(User user) {
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(user);
+    }
+
+    @Override
+    public void deleteUser(User user) {
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(user);
+    }
+
     @Override
     public User getUserByLogin(String login) {
         return null;
+    }
+
+    @Override
+    public List getAllUsers() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from User").list();
     }
 }
